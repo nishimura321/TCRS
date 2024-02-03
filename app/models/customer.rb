f97ee0fd966bbd232810061502c5206d2e88b0a4
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-02-03 16:40:43 +0000

    エラー修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,18 +4,18 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum breastfeeding_status_method: { no_selection: 0, breast_milk_only: 1, breast_milk_and_formula_milk: 2, formula_milk_only: 3, follow_up_milk: 4, milk: 5 }
-  enum situation_after_breastfeeding_method: { no_selection: 0, easy_to_vomit_milk: 1, difficult_to_vomit_milk: 2, none_of_the_above: 3 }
-  enum milk_situation_after_baby_food_method: { no_selection: 0, drink_breast_milk: 1, drink_formula_milk: 2, not_drinking: 3 }
-  enum baby_food_progress_method: { no_selection: 0, early_weaning_period: 1, middle_weaning_period: 2, latter_weaning_period: 3, the_weaning_completion_period: 4, normal_food: 5 }
-  enum appetite_method: { no_selection: 0, eat_a_lot: 1, eat_a_standard_amount: 2, children_do_not_eat_much: 3, eating_unevenly: 4 }
-  enum how_to_eat_method: { no_selection: 0, feed_a_child: 1, eat_with_one’s_hand: 2, eat_alone_with_a_spoon: 3, eat_with_hands_and_spoon: 4, eat_with_chopsticks: 5 }
+  enum breastfeeding_status_method: { breast_milk_only: 0, breast_milk_and_formula_milk: 1, formula_milk_only: 2, follow_up_milk: 3, milk: 4 }
+  enum situation_after_breastfeeding_method: { easy_to_vomit_milk: 0, difficult_to_vomit_milk: 1, none_of_the_above: 2 }
+  enum milk_situation_after_baby_food_method: { drink_breast_milk: 0, drink_formula_milk: 1, not_drinking: 2 }
+  enum baby_food_progress_method: { early_weaning_period: 0, middle_weaning_period: 1, latter_weaning_period: 2, the_weaning_completion_period: 3, normal_food: 4 }
+  enum appetite_method: { eat_a_lot: 0, eat_a_standard_amount: 1, children_do_not_eat_much: 2, eating_unevenly: 3 }
+  enum how_to_eat_method: { feed_a_child: 0, eat_with_one’s_hand: 1, eat_alone_with_a_spoon: 2, eat_with_hands_and_spoon: 3, eat_with_chopsticks: 4 }
   enum how_to_lay_it_down_method: { alone: 0, co_sleeping: 1, baby_carrying: 2, cradling: 3 }
-  enum falling_asleep_method: { no_selection: 0, falling_asleep_easily: 1, have_trouble_sleeping: 2, there_are_both: 3 }
-  enum waking_up_method: { no_selection: 0, waking_up_easily: 1, unable_to_get_to_sleep: 2, there_are_both: 3 }
+  enum falling_asleep_method: { falling_asleep_easily: 0, have_trouble_sleeping: 1, there_are_both: 2 }
+  enum waking_up_method: { waking_up_easily: 0, unable_to_get_to_sleep: 1, both_0_and_1: 2 }
   enum sleeping_position_method: { sleep_on_your_back: 0, sleep_on_your_side: 1, sleep_on_your_stomach: 2 }
-  enum diaper_usage_method: { no_selection: 0, diaper: 1, cloth_diaper: 2, paper_pants: 3, cloth_pants: 4 }
-  enum urination_method: { no_selection: 0, pee_in_omar: 1, pee_in_the_toilet: 2, tell_me_after_peeing_in_the_pants: 3, do_not_tell: 4, pee_by_myself: 5 }
-  enum defecation_method: { no_selection: 0, hard_poop: 1, normal_poop: 2, soft_poop: 3, feeling_constipated: 4 }
+  enum diaper_usage_method: { diaper: 0, cloth_diaper: 1, paper_pants: 2, cloth_pants: 3 }
+  enum urination_method: { pee_in_omar: 0, pee_in_the_toilet: 1, tell_me_after_peeing_in_the_pants: 2, do_not_tell: 3, pee_by_myself: 4 }
+  enum defecation_method: { hard_poop: 0, normal_poop: 1, soft_poop: 2, feeling_constipated: 3 }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_26_144500) do
+ActiveRecord::Schema.define(version: 2024_01_25_133601) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,54 +22,6 @@ ActiveRecord::Schema.define(version: 2024_01_26_144500) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "children", force: :cascade do |t|
-    t.integer "cusutomer_id", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.date "birthday", null: false
-    t.boolean "male", null: false
-    t.decimal "normal_body_temperature"
-    t.decimal "fever"
-    t.boolean "has_febrile_seizure", null: false
-    t.text "febrile_seizure_details"
-    t.boolean "has_dislocation", null: false
-    t.text "dislocation_details"
-    t.boolean "has_allergy", null: false
-    t.text "allergy_details"
-    t.integer "breastfeeding_status"
-    t.integer "breastfeeding_amount"
-    t.integer "situation_after_breastfeeding"
-    t.integer "milk_situation_after_baby_food"
-    t.integer "amount_of_milk_after_baby_food"
-    t.integer "baby_food_progress"
-    t.integer "appetite"
-    t.string "favorite_food"
-    t.string "hated_food"
-    t.integer "how_to_eat"
-    t.integer "meal_time"
-    t.integer "how_to_lay_it_down"
-    t.integer "falling_asleep"
-    t.integer "waking_up"
-    t.integer "sleeping_position"
-    t.boolean "is_deep_sleep"
-    t.string "habits_when_sleeping"
-    t.integer "diaper_usage"
-    t.boolean "is_sensitive_to_rash"
-    t.integer "urination"
-    t.integer "defecation"
-    t.string "number_of_defecation"
-    t.text "favorite_game"
-    t.text "favorite_toy"
-    t.string "hospital_name"
-    t.string "hospital_telephone_number"
-    t.text "memo"
-    t.boolean "is_active", default: true, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -119,69 +71,6 @@ ActiveRecord::Schema.define(version: 2024_01_26_144500) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_facilities_on_email", unique: true
     t.index ["reset_password_token"], name: "index_facilities_on_reset_password_token", unique: true
-  end
-
-  create_table "families", force: :cascade do |t|
-    t.integer "cusutomer_id", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.boolean "male", null: false
-    t.integer "relationship", null: false
-    t.date "birthday", null: false
-    t.string "telephone_number", null: false
-    t.string "workplace"
-    t.string "workplace_telephone_number"
-    t.boolean "is_active", default: true, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "family_reservations", force: :cascade do |t|
-    t.integer "family_id", null: false
-    t.integer "reservation_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "menus", force: :cascade do |t|
-    t.date "date", null: false
-    t.text "school_lunch"
-    t.string "snack"
-    t.text "ingredient", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id", null: false
-    t.integer "followed_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reservation_conditions", force: :cascade do |t|
-    t.date "unavailable_days"
-    t.time "unavailable_times"
-    t.integer "maximum_number_of_people"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.integer "cusutomer_id", null: false
-    t.integer "child_id", null: false
-    t.integer "facility_id", null: false
-    t.integer "menu_id", null: false
-    t.date "date", null: false
-    t.time "time", null: false
-    t.boolean "wants_meal_service", null: false
-    t.integer "purpose_of_use", null: false
-    t.boolean "is_valid_reservation", default: true, null: false
-    t.boolean "is_allergy_checked", default: true, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
