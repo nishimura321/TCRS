980d89a36c425c1d361eda8ddf827c3594c6f6c4
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-02-07 15:01:26 +0000

    マイページ修正

--- app/helpers/customer/children_helper.rb
+++ app/helpers/customer/children_helper.rb
@@ -1,2 +1,25 @@
 module Customer::ChildrenHelper
+
+#年齢を返すメソッド
+def age_in_years(birthday)
+  now = Time.now
+  age = now.year - birthday.year
+  age -= 1 if now.month < birthday.month || (now.month == birthday.month && now.day < birthday.day)
+  age
+end
+
+#月齢を返すメソッド
+def age_in_months(birthday)
+  now = Time.now
+  if now.month >= birthday.month
+    age_in_months = now.month - birthday.month
+  else
+    age_in_months = (12 - birthday.month) + now.month
+  end
+  age_in_months -= 1 if now.day < birthday.day
+  #以下は月齢がマイナスになる場合月齢を0にするための処理
+  age_in_months = 0 if age_in_months < 0
+  age_in_months
+end
+
 end
