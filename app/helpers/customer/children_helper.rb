96434bd81bb67340f29292c085c3473f7e3d4ed8
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-02-09 14:01:36 +0000

    enum表示メソッド定義

--- app/helpers/customer/children_helper.rb
+++ app/helpers/customer/children_helper.rb
@@ -22,4 +22,52 @@ def age_in_months(birthday)
   age_in_months
 end
 
+def  breastfeeding_status_i18n_text(child)
+  if child.breast_milk_only?
+    I18n.t("enums.child.breastfeeding_status.breast_milk_only")
+  elsif child.breast_milk_and_formula_milk?
+    I18n.t("enums.child.breastfeeding_status.breast_milk_and_formula_milk")
+  elsif child.formula_milk_only?
+    I18n.t("enums.child.breastfeeding_status.formula_milk_only")
+  elsif child.follow_up_milk?
+    I18n.t("enums.child.breastfeeding_status.follow_up_milk")
+  elsif child.milk?
+    I18n.t("enums.child.breastfeeding_status.milk")
+  end
+end
+
+def  situation_after_breastfeeding_i18n_text(child)
+  if child.easy_to_vomit_milk?
+    I18n.t("enums.child.situation_after_breastfeeding.easy_to_vomit_milk")
+  elsif child.difficult_to_vomit_milk?
+    I18n.t("enums.child.situation_after_breastfeeding.difficult_to_vomit_milk")
+  elsif child.none_of_the_above?
+    I18n.t("enums.child.situation_after_breastfeeding.none_of_the_above")
+  end
+end
+
+def  milk_situation_after_baby_food_i18n_text(child)
+  if child.drink_breast_milk?
+    I18n.t("enums.child.milk_situation_after_baby_food.drink_breast_milk")
+  elsif child.drink_formula_milk?
+    I18n.t("enums.child.milk_situation_after_baby_food.drink_formula_milk")
+  elsif child.not_drinking?
+    I18n.t("enums.child.milk_situation_after_baby_food.not_drinking")
+  end
+end
+
+def  baby_food_progress_i18n_text(child)
+  if child.early_weaning_period?
+    I18n.t("enums.child.baby_food_progress.early_weaning_period")
+  elsif child.middle_weaning_period?
+    I18n.t("enums.child.baby_food_progresss.middle_weaning_period")
+  elsif child.latter_weaning_period?
+    I18n.t("enums.child.baby_food_progress.latter_weaning_period")
+  elsif child.the_weaning_completion_period?
+    I18n.t("enums.child.baby_food_progress.the_weaning_completion_period")
+  elsif child.normal_food?
+    I18n.t("enums.child.normal_food")
+  end
+end
+
 end
