aac653b7ae162282bc675dfa6797802d7a824a90
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-02-13 13:24:02 +0000

    [Add]シンプルカレンダー

--- app/controllers/customer/reservations_controller.rb
+++ app/controllers/customer/reservations_controller.rb
@@ -1,7 +1,10 @@
 class Customer::ReservationsController < ApplicationController
   before_action :authenticate_customer!
+  before_action :ensure_reservation, only: [:show, :edit, :update, :destroy]
 
   def new
+    @reservation = Reservation.new(customer_id: current_customer.id)
+    @reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
   end
 
   def confirm
@@ -31,4 +34,13 @@ class Customer::ReservationsController < ApplicationController
   def destroy
   end
 
+  private
+  def reservation_params
+    params.require(:reservation).permit(:date, :time, :wants_meal_service, :purpose_of_use)
+  end
+
+  def ensure_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+  
 end
