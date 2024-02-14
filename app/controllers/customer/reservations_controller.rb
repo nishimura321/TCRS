cd4939857a73679a34c38d7ef2758c77a7ae1925
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-02-14 16:19:44 +0000

    [Add]予約情報入力画面

--- app/controllers/customer/reservations_controller.rb
+++ app/controllers/customer/reservations_controller.rb
@@ -6,7 +6,7 @@ class Customer::ReservationsController < ApplicationController
     start_date = Date.current
     end_date = start_date.end_of_month.next_month
     @reservations = Reservation.all.where("day >= ? AND day <= ?", start_date, end_date).order(day: :desc)
-    @reservation = Reservation.new(customer_id: current_customer.id)
+    @reservation = Reservation.new
     
   end
 
@@ -17,6 +17,14 @@ class Customer::ReservationsController < ApplicationController
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      flash[:notice] = "ご予約が完了しました。"
+      redirect_to reservations_thanks_path
+    else
+      flash.now[:notice] = "新規予約ができませんでした。"
+      render reservations_confir_path
+    end
   end
 
   def index
@@ -39,7 +47,7 @@ class Customer::ReservationsController < ApplicationController
 
   private
   def reservation_params
-    params.require(:reservation).permit(:date, :time, :wants_meal_service, :purpose_of_use)
+    params.require(:reservation).permit(:day, :time, :wants_meal_service, :purpose_of_use, :start_time)
   end
 
   def ensure_reservation
