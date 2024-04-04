803572ef72714d87b87ffdca93410112a2b6c875
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-04-04 12:27:48 +0000

    [Fix]search function

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -11,14 +11,14 @@ class Menu < ApplicationRecord
   #検索方法
   def self.search_for(word, search, facility)
     if search == 'perfect_match'
-      Menu.where(date: word)
+      facility.menus.where(date: word)
     elsif search == 'forward_match'
-      Menu.where('date LIKE ?', word + '%')
+      facility.menus.where('date LIKE ?', word + '%')
     elsif search == 'backward_match'
-      Menu.where('date LIKE ?', '%' + word)
+      facility.menus.where('date LIKE ?', '%' + word)
     #以下は部分一致
     else
-      Menu.where('date LIKE ?', '%' + word + '%')
+      facility.menus.where('date LIKE ?', '%' + word + '%')
     end
   end
 
